8780ac403d25e18d90eda059885d7021527d453a
Jultis <ajulton@gmail.com>
2017-10-04 19:00:12 +0200

    Skeleton for script file. Needs work

---
+++ Project/Main/ref_stars.py
@@ -0,0 +1,11 @@
+from ast2000solarsystem_27_v4 import AST2000SolarSystem as A2000
+
+class Scripts(object):
+
+    def __init__(self, seed = None):
+        if seed == None:
+            seed = 45360
+        self.mySolarSystem = A2000(seed)
+
+    def get_lambda_deg_from_ref_stars(self):
+        self.mySolarSystem.get_ref_stars()
